31bdf42fd2ea84297498c79cd7c161b6c97ad5ae
olehratinskiy <oleh.ratinskyi.knm.2020@lpnu.ua>
2022-06-06 15:41:19 +0300

    change code style

--- App/src/Message.js
+++ App/src/Message.js
@@ -1,11 +1,11 @@
-import React from "react";
+import React from 'react';
 
 
 function Message(props) {
     return (
-        <div className="msg">
-            <p className="msg-text">Error {props.code}. {props.state}.</p>
-            <button className="msg-btn" type="button" onClick={(props.handleClose)}>Close</button>
+        <div className='msg'>
+            <p className='msg-text'>Error {props.code}. {props.state}.</p>
+            <button className='msg-btn' type='button' onClick={(props.handleClose)}>Close</button>
         </div>
     );
 }
