29326b220fc593ba4015dc47f44c5c801ee29f63
olehratinskiy <oleh.ratinskyi.knm.2020@lpnu.ua>
2022-06-06 22:00:34 +0300

    add admin functions

---
+++ App/src/ProductDeletion.js
@@ -0,0 +1,102 @@
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
+import {useLocation} from 'react-router-dom';
+import './styles/style.css';
+import Navbar from './Navbar';
+import Footer from './Footer';
+import backBtn from './images/back-button-icon.png';
+import Message from './Message';
+
+
+function ProductDeletion (props) {
+    const location = useLocation();
+    const [products, setProducts] = useState(location.state.products);
+    const [code, setCode] = useState(-1);
+    const [state, setState] = useState('');
+    const [token, setToken] = useState(localStorage.getItem('token'));
+    const [selectedRowId, setSelectedRowId] = useState(-1);
+
+    const handleClose = () => {
+        setCode(-1);
+        setState('');
+    }
+
+    const showMessage = () => {
+        if (code !== 200 && code !== -1) {
+            return (<Message handleClose={handleClose} code={code} state={state.message}/>);
+        } else return null;
+    }
+
+    const deleteProduct = () => {
+        setToken(localStorage.getItem('token'))
+        fetch(`http://127.0.0.1:5000/item/${selectedRowId}`, {
+            method: 'DELETE',
+            headers: {
+            'Authorization': 'Bearer ' + token
+        },
+        }).then(response => {
+            if (response.status === 200)
+            {
+                setProducts(
+                    products.filter(function( obj ) {
+                        return obj.item_id !== selectedRowId;
+                    })
+                );
+                return response.json();
+            }
+            else {
+                setCode(response.status);
+                response.json().then( message => setState(message) );
+            }
+        }).catch(error => {
+            return error;
+        });
+    }
+
+    return (
+        <div>
+            {Navbar()}
+            {showMessage()}
+            <div className='main' id='delete'>
+                <div className='admin-full-delete login-full'>
+                    <div className='admin-inner-delete login-inner'>
+                        <div className='title-section'>
+                            <Link to={'/admin'}><img className='back-btn' src={backBtn} alt=''/></Link>
+                            <h1 className='verification-text'>Delete Item</h1>
+                        </div>
+
+                        <table id='table'>
+                            <tbody>
+                                <tr>
+                                    <td>ID</td>
+                                    <td>Name</td>
+                                    <td>Quantity</td>
+                                    <td>Price</td>
+                                    <td>Status</td>
+                                </tr>
+
+                                {products ? products.map( ( {item_id, name, storage_quantity, price, status} ) => {
+                                    return (
+                                        <tr className={item_id === selectedRowId ? 'selected' : null} key={item_id} onClick={(e) => {
+                                            e.target.parentNode.className === 'selected' ? setSelectedRowId(-1) : setSelectedRowId(item_id)
+                                        }}>
+                                            <td className='row-data'>{item_id}</td>
+                                            <td className='row-data'>{name}</td>
+                                            <td className='row-data'>{storage_quantity}</td>
+                                            <td className='row-data'>{price} UAH</td>
+                                            <td className='row-data'>{status}</td>
+                                        </tr>
+                                    )
+                                }) : null}
+                            </tbody>
+                        </table>
+                        <input type='button' className='more-btn' value='DELETE' onClick={deleteProduct}/>
+                    </div>
+                </div>
+            </div>
+            {Footer()}
+        </div>
+    )
+}
+
+export default ProductDeletion;
---
+++ App/src/ProductEdition.js
@@ -0,0 +1,100 @@
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
+import './styles/style.css'
+import Navbar from './Navbar';
+import Footer from './Footer';
+import backBtn from './images/back-button-icon.png';
+import Message from './Message';
+
+
+function ProductEdition () {
+    const [code, setCode] = useState(-1);
+    const [state, setState] = useState('');
+    const [token, setToken] = useState(localStorage.getItem('token'));
+    const [messageShow, setMessageShow] = useState(false);
+    const [name, setName] = useState('');
+    const [storageQuantity, setStorageQuantity] = useState('');
+    const [price, setPrice] = useState('');
+
+
+    const handleClose = () => {
+        setCode(-1);
+        setState('');
+        setMessageShow(false);
+    }
+
+    const showMessage = () => {
+        if (code !== 200 && code !== -1) {
+            return (<Message handleClose={handleClose} code={code} state={state.message}/>);
+        } else return null;
+    }
+
+    const editProduct = () => {
+        setToken(localStorage.getItem('token'));
+        fetch(`http://127.0.0.1:5000/item/${name !== '' ? name : 'test'}`, {
+            method: 'PUT',
+            headers: {
+                'Authorization': 'Bearer ' + token,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                name: name,
+                storage_quantity: storageQuantity,
+                price: price,
+                status: 'available'
+            }),
+            }).then(response => {
+                if (response.status === 200) {
+                    setName('');
+                    setStorageQuantity('');
+                    setPrice('');
+                    return response.json();
+                }
+                else {
+                    setCode(response.status);
+                    response.json().then( message => setState(message) )
+
+                    setMessageShow(true)
+                }
+            }).catch(error => {
+                return error;
+            });
+        }
+
+    return (
+        <div>
+            {Navbar()}
+            {showMessage()}
+            <div className='main'>
+                <div className='login-full'>
+                    <div className='add-item-inner login-inner'>
+                        <div className='title-section'>
+                            <Link to={'/admin'}><img className='back-btn' src={backBtn} alt=''/></Link>
+                            <h1 className='verification-text'>Update Item</h1>
+                        </div>
+                        <div className='add-item-input-wrapper login-input-wrapper'>
+                            <div className='login-input-container'>
+                                <label htmlFor='name' className='input-field-name'>Name</label>
+                                <input disabled={messageShow} value={name} id='name' className='input-field' size='30' onChange={(e) => {setName(e.target.value)}} required/>
+                            </div>
+                            <div className='login-input-container'>
+                                <label htmlFor='quantity' className='input-field-name'>Quantity</label>
+                                <input disabled={messageShow} value={storageQuantity} id='quantity' className='input-field' size='30' onChange={(e) => {setStorageQuantity(e.target.value)}} required/>
+                            </div>
+                            <div className='login-input-container'>
+                                <label htmlFor='price' className='input-field-name'>Price</label>
+                                <input disabled={messageShow} value={price} id='price' className='input-field' size='30' onChange={(e) => {setPrice(e.target.value)}} required/>
+                            </div>
+                        </div>
+                        <div className='admin-btn login-btn-wrapper'>
+                            <button className='login-btn' onClick={editProduct}>UPDATE</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {Footer()}
+        </div>
+    )
+}
+
+export default ProductEdition;
